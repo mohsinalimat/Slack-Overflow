00fdcd94cec1b57519487f2c6e9a8c55f8d5c577
alwongg <wong_1994@hotmail.com>
2017-09-21 12:41:21 -0400

    Log out method implemented with Guard statement

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ChatViewController: UIViewController {
     
@@ -105,7 +105,16 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
-        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error signing out")
+        }
+        guard (navigationController?.popToRootViewController(animated: true)) != nil
+            else {
+                print("No view controllers to pop off")
+                return
+        }
         
     }
     
