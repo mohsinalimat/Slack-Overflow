cf1221b352fdfacaf2416719c312139614cb80c4
alwongg <wong_1994@hotmail.com>
2017-09-21 15:36:20 -0400

    UI Animations with textfields

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // MARK: - Properties
 
@@ -31,11 +31,12 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         //TODO: Set yourself as the delegate of the text field here:
 
-        
+        messageTextfield.delegate = self
         
         //TODO: Set the tapGesture here:
         
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
 
         //TODO: Register your MessageCell.xib file here:
 
@@ -71,7 +72,9 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Declare tableViewTapped here:
     
-    
+    @objc func tableViewTapped(){
+        messageTextfield.endEditing(true)
+    }
     
     //TODO: Declare configureTableView here:
     
@@ -89,12 +92,23 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Declare textFieldDidBeginEditing here:
     
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
     
     //TODO: Declare textFieldDidEndEditing here:
     
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
     
     ///////////////////////////////////////////
     
--- Flash Chat/Message.swift
+++ Flash Chat/Message.swift
@@ -10,6 +10,7 @@ class Message {
     
     //TODO: Messages need a messageBody and a sender variable
     
-    
+    var sender: String = ""
+    var messageBody: String = ""
     
 }
