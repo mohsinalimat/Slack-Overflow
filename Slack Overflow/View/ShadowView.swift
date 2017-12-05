48ad0edaa9a1b1af10083dd5c79f600d666db81d
alwongg <wong_1994@hotmail.com>
2017-12-04 21:37:10 -0600

    Create shadowview custom class for UIView

--- Slack Overflow/View/ShadowView.swift
+++ Slack Overflow/View/ShadowView.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ShadowView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
     }
-    */
-
+    
+    func setupView(){
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
 }
